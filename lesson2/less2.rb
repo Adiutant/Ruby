32e87be352b1899e3b9fdc57fb46adab54ff86d2
Adiutant <lagynkov.rom@gmail.com>
2018-04-08 11:22:25 +0300

    1

--- lesson2/less2.rb
+++ lesson2/less2.rb
@@ -1,5 +1,8 @@
 puts "hello, whats ur name?"
 name = gets.chomp
-puts "ur name is " + name + ", how old are u?"
+puts "ur name is " + name.capitalize + ", how old are u?"
 age = gets.chomp
-puts name + ", you are " + age + " years old"
+puts name.capitalize + ", you are " + age + " years old"
+string = name.capitalize + ", you are " + age + " years old"
+puts string.reverse
+puts age.to_i + name.length

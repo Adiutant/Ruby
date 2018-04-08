591873613a93f6ec3de88e3e963c319d22ca660f
Adiutant <lagynkov.rom@gmail.com>
2018-04-08 13:29:52 +0300

    1

--- lesson2/less2.rb
+++ lesson2/less2.rb
@@ -1,9 +1,16 @@
+
 puts "hello, whats ur name?"
 name = gets.chomp
-puts "ur name is " + name.capitalize + ", how old are u?"
+def n(name)
+puts "ur name is #{name.capitalize}, how old are u?"
+end
+n(name)
 age = gets.chomp
-puts name.capitalize + ", you are " + age + " years old"
+def a(name,age)
+puts "#{name.capitalize}, you are #{age} years old"
+end
+a(name,age)
 string = name.capitalize + ", you are " + age + " years old"
 puts string.reverse
 puts age.to_i + name.length
-puts "Good bye"
+puts "Good bye" 

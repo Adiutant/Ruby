a4004c94d89bf97fc1abacdfc23485d4b5fe38e1
Adiutant <lagynkov.rom@gmail.com>
2018-04-09 21:16:24 +0300

    1

--- lesson3/less3.rb
+++ lesson3/less3.rb
@@ -1,13 +1,14 @@
 puts "Print number"
 num = gets.chomp.to_i
 def n(num) 
-c = 0
+c = 1
 n = 1
 while num != (num % 10 ** n) 
 	c += 1
 	n += 1
 end
-print c
+puts c
 return  num % 10
 end
-n(num)
+result = n(num)
+puts result

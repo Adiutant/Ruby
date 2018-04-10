93127522b28663d44fca8ab67210b4da58e989be
Adiutant <lagynkov.rom@gmail.com>
2018-04-10 19:15:57 +0300

    1

--- lesson3/ordercheck.rb
+++ lesson3/ordercheck.rb
@@ -1,19 +1,20 @@
 #massive order check #wrong 10 
+def order(a,x)
+while x < 3
+if (a[x]) < (a[x + 1]) then
+ch = "true"
+else
+ch = "false"
+end
+x += 1
+end
+return ch
+end
 a = Array.new
 n = 0
 while n < 5
 	a[n] = gets.chomp.to_i
 	n += 1
 end
-ch = "true"
-def order(a,n)
-while n > 0
-if (a[n]) > (a[n - 1]) then
-ch = "true"
-else
-ch = "false"
-end
-end
-end
-order(a,n)
-puts ch
+s = order(a,0)
+puts s

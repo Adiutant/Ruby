0dd5a986ed0bfa1d0549937ad7abea0d257346a0
Adiutant <lagynkov.rom@gmail.com>
2018-04-10 19:25:59 +0300

    1

---
+++ lesson3/arrayMinus.rb
@@ -0,0 +1,18 @@
+def m(a,b)
+return	a - b
+end
+a = Array.new
+b = Array.new
+n = 0
+while n < 5
+	a[n] = gets.chomp.to_i
+	n += 1
+end
+n = 0
+puts "input massive 2"
+while n < 2
+	b[n] = gets.chomp.to_i
+	n += 1
+end
+s = m(a,b)
+puts s
